f7404595727dd874893ae67c5f612b3262d2e059
Bircheses <kbrzoza03@outlook.com>
2024-12-19 23:48:10 +0100

    Initial commit

---
+++ SimulatedAnnealing.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Szef on 16.12.2024.
+//
+
+#include "SimulatedAnnealing.h"
---
+++ SimulatedAnnealing.h
@@ -0,0 +1,16 @@
+//
+// Created by Szef on 16.12.2024.
+//
+
+#ifndef SIMULATEDANNEALING_H
+#define SIMULATEDANNEALING_H
+
+
+
+class SimulatedAnnealing {
+
+};
+
+
+
+#endif //SIMULATEDANNEALING_H
